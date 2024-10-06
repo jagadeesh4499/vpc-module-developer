b18986b67333534de45fbd8a6ce65ab1dfedf1e3
jagadeesh <jagadeeshakula123@gmail.com>
2024-10-06 13:00:05 +0530

    vpc-module-developer

--- outputs.tf
+++ outputs.tf
@@ -14,7 +14,7 @@ output "database_subnet_ids" {
   value = aws_subnet.database[*].id
 }
 
-output "database_subnet_group" {
+output "database_subnet_group_name" {
   value = aws_db_subnet_group.default.name
 }
 
